fb8a0aed9e89d19d77c0f8dd54017b7750890f7d
Timothy J Fontaine <tjfontaine@gmail.com>
2015-06-26 10:37:25 -0700

    allow tests to disable webirc

--- webirc.js
+++ webirc.js
@@ -7,13 +7,14 @@ function WebIRC(client, config) {
   if (!(this instanceof WebIRC))
     return new WebIRC(client, config);
 
-  stream.Transform.call(this, {
-    objectMode: true,
-    decodeStrings: false,
-    highWaterMark: 0,
-  });
+  stream.Transform.call(this, {});
 
   this.webirc_firstLine = false;
+
+  // coerce to number and treat as boolean
+  // undefined, 0, and NaN will result in false
+  // otherwise true
+  this.webirc_disabled = !!(+process.env.WEBIRC_DISABLED);
 
   this.webirc_password = config.destination.webirc_password || 'secret';
   this.webirc_ip = client.remoteAddress;
@@ -24,7 +25,7 @@ util.inherits(WebIRC, stream.Transform);
 
 WebIRC.prototype._transform = function webircTransform(chunk, enc, cb) {
   // We have already sent the WEBIRC command, fast path
-  if (!this.webirc_firstLine) {
+  if (!this.webirc_disabled && !this.webirc_firstLine) {
     this.webirc_firstLine = true;
 
     var msg = [
@@ -32,10 +33,13 @@ WebIRC.prototype._transform = function webircTransform(chunk, enc, cb) {
       this.webirc_password,
       this.webirc_host,
       this.webirc_ip,
-      this.webirc_cloak,
+      this.webirc_cloak, // TODO new field for the module
     ].join(' ');
 
+    console.error('WEBIRC', msg);
     this.push(msg);
+
+    // TODO pass along CERTFP
   }
 
   this.push(chunk);
